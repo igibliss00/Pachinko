72596f32b9d56addba63a09a2ee3679a4fb95aa7
igibliss00 <igibliss00@gmail.com>
2020-06-27 14:38:55 -0400

    Initial commit

---
+++ Project11/GameScene.swift
@@ -0,0 +1,34 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by jc on 2020-06-26.
+//  Copyright © 2020 J. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+            
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        box.position = location
+        addChild(box)
+    }
+}
+
+
